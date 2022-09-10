6c6f3595a60e379fb7bb921cfda84588690d8281
Avinava Basu <avinava@mycelium.ventures>
2022-09-10 14:13:07 +0530

    Boot disk size change for OCR nodes

--- OCR/bsc/variables.tf
+++ OCR/bsc/variables.tf
@@ -2,3 +2,4 @@ variable "machine_type" {}
 variable "node_name" {}
 variable "node_type" {}
 variable "project_id" {}
+variable "boot_disk_size" {}
--- OCR/xdai/terraform.tfvars
+++ OCR/xdai/terraform.tfvars
@@ -2,3 +2,4 @@ machine_type = "n1-standard-1"
 node_name = "xdai"
 node_type = "ocr"
 project_id = "avian-direction-235610"
+boot_disk_size = 30
